11e40b2325db2f985c169fc7a0039118cd14e8b8
Joe Baker <joe.baker@thenational.academy>
2026-08-06 13:42:17 +0100

    chore: remove unknown type

--- apps/api/app/trpc/createTrpcRouteHandler.ts
+++ apps/api/app/trpc/createTrpcRouteHandler.ts
@@ -1,4 +1,4 @@
-import type { AnyRouter } from "@trpc/server";
+import type { AnyRouter, inferRouterContext } from "@trpc/server";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { NextResponse, type NextRequest } from "next/server";
 import { raLogger } from "@oaknational/resource-adapter-logger";
@@ -20,7 +20,9 @@ interface TrpcRouteConfig<TRouter extends AnyRouter> {
   router: TRouter;
   endpoint: string;
   log: ReturnType<typeof raLogger>;
-  createContext: (req: Request) => Promise<unknown> | unknown;
+  createContext: (
+    req: Request,
+  ) => inferRouterContext<TRouter> | Promise<inferRouterContext<TRouter>>;
 }
 
 export function createTrpcRouteHandler<TRouter extends AnyRouter>(
